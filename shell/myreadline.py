0b3eef312c7ce763f3b53cef8b4777e496646d1d
ZanteZyaire <jazamora6@miners.utep.edu>
2021-02-24 01:04:23 -0700

    Fixed variable buff to match entire program

--- shell/myreadline.py
+++ shell/myreadline.py
@@ -4,12 +4,12 @@ from os import read, write
 def myreadline():
     index = 0
     line = ""
-    buff = read(0, 100)
-    string = buff.decode()
+    buff = read(0, 100) # Buffer set with limit 100 
+    string = buff.decode() # makes string interpretable
     
     while index < len(string):
         current_char = string[index]
-        if current_char == '\n':
+        if current_char == '\n': # checks for newline char
             return line
         
         line += current_char
@@ -17,7 +17,7 @@ def myreadline():
         
         if index == 100:
             buff =  read(0,100)
-            string = ibuf.decode()
+            string = buff.decode()
             index = 0
-            
+
     return "" # EOF reached
