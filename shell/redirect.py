f923451ed7d22dad9eae06362f1f77902b19c2a8
ZanteZyaire <jazamora6@miners.utep.edu>
2021-03-05 22:53:54 -0700

    fixed argument splitting

--- shell/redirect.py
+++ shell/redirect.py
@@ -5,12 +5,12 @@ import sys
 def redirect(arg, symbol):
     if symbol == '<' and arg.count("<") == 1:
         os.close(0)
-        os.open(arg.pop(arg.index(symbol)), os.O_RDONLY)
+        os.open(arg[arg.index(symbol) + 1], os.O_RDONLY)
         os.set_inheritable(0, True)
 
     elif symbol == '>' and arg.count(">") == 1:
         os.close(1)
-        os.open(arg.pop(arg.index(symbol)), os.O_CREAT | os.O_WRONLY)
+        os.open(arg[arg.index(symbol) + 1], os.O_CREAT | os.O_WRONLY)
         os.set_inheritable(1, True)
 
     else:
